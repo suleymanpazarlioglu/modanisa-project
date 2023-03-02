11d06a20dd37a92bb5c38dda3939e709c9615657
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-03-02 17:42:57 +0300

    failed test runner

---
+++ src/test/java/com/modanisa/project/runners/FailedTestRunner.java
@@ -0,0 +1,16 @@
+package com.modanisa.project.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/modanisa/project/step_definitions"
+)
+
+
+public class FailedTestRunner {
+
+}
